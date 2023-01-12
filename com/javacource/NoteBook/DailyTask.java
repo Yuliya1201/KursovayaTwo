9dae915bc5ec9c91c4c803abfadb6bcc191c5d2f
YULIYA <114095662+Yuliya1201@users.noreply.github.com>
2023-01-12 23:49:52 +0300

    Update DailyTask.java

--- com/javacource/NoteBook/DailyTask.java
+++ com/javacource/NoteBook/DailyTask.java
@@ -10,6 +10,6 @@ public class DailyTask extends Task implements Repeatable {
 
     @Override
     public boolean checkOccurance(LocalDateTime requestedDate) {
-        return getFirstDate().toLocalDate().equals(requestedDate.toLocalDate());
+        return getFirstDate();
     }
 }
